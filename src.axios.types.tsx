c90c79dd18684c9a1c16983014b907f62ad605f0
zhangyanling77 <18783226594@163.com>
2019-11-11 17:52:48 +0800

    Update src.axios.types.tsx

--- src.axios.types.tsx
+++ src.axios.types.tsx
@@ -1,2 +1,14 @@
-
+// 请求方法
 export type Methods = 'get' | 'GET' | 'post' | 'POST' | 'put' | 'PUT' | 'delete' | 'DELETE' | 'options' | 'OPTIONS';
+
+export interface AxiosRequestConfig {
+  url: string;
+  method: Methods; 
+  params: Record<string, any> // 代表一个对象，属性是string，值是任何值
+}
+
+// 这个接口用来修饰Axios.prototype.request方法
+// Promise的T代表此promise变成成功态后resolve的值 => resolve(T)
+export interface AxiosInstance {
+  <T = any>(config: AxiosRequestConfig): Promise<T>
+}
