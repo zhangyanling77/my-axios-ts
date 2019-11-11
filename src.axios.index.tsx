03f0ea1468c2b02f48c79668ecf4c8490f9c5c96
zhangyanling77 <18783226594@163.com>
2019-11-11 17:35:28 +0800

    Update src.axios.index.tsx

--- src.axios.index.tsx
+++ src.axios.index.tsx
@@ -1,8 +1,9 @@
 import Axios from './Axios.tsx';
-
 // 创建axios实例
+// 定义一个类，类的原型，类的实例
+
 function createInstance(){
-  let context = new Axios() // this，上下文
+  let context: Axios = new Axios() // this，上下文
   // 让request里的方法里的this永远指向context
   let instance = Axios.prototype.request.bind(context);
   // 把axios类的实例和类的原型上的方法都拷贝到instance上，即request方法上
