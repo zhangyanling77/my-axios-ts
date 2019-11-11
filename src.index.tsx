68d984285270a123a3d892e16190764a76ad9646
zhangyanling77 <18783226594@163.com>
2019-11-11 17:19:48 +0800

    feature: axios 官方库的使用

---
+++ src.index.tsx
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from 'axios';
+
+const baseURL = 'http://localhost:8080';
+// 服务器返回的对象
+interface User {
+  name: string;
+  password: string;
+}
+let user:User = {
+  name: 'zhangsan',
+  password: '123456'
+}
+
+/*
+* 先看一下怎么使用官方库
+*/
+axios({
+  method: 'get', // 方法名
+  url: baseURL, // 访问路径
+  params: user // 查询参数对象，它会转成查询字符串放在？后面
+}).then((response: AxiosResponse) => {
+  console.log(response)
+  return response.data
+}).catch((error: any) => {
+  console.log(error)
+  // throw Error(error)
+})
