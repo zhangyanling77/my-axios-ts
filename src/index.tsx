84b4e1fa7f648e771133090f7441c6955d50091b
zhangyanling77 <18783226594@163.com>
2019-11-12 10:26:56 +0800

    post请求

--- src/index.tsx
+++ src/index.tsx
@@ -16,13 +16,17 @@ let user:User = {
 * 先看一下怎么使用官方库
 */
 axios({
-  method: 'get', // 方法名
-  url: baseURL + '/get', // 访问路径
-  params: user // 查询参数对象，它会转成查询字符串放在？后面
-}).then((response: AxiosResponse) => {
+  method: 'post', //'get', // 方法名
+  url: baseURL + '/post', //'/get', // 访问路径
+  // params: user // 查询参数对象，它会转成查询字符串放在？后面
+  headers: {
+    'content-type': 'application/json',
+  }
+}).then((response: AxiosResponse<User>) => {
   console.log(response)
-  return response.data
-}).catch((error: any) => {
+  conosle.log(response.data)
+  // return response.data
+})
+.catch((error: any) => {
   console.log(error)
-  // throw Error(error)
 })
