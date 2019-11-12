a1eaea64f700f0b64bbbc5783ad6ef2a9fcd39b9
zhangyanling77 <18783226594@163.com>
2019-11-12 10:39:06 +0800

    错误处理

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 // import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
-import axios, { AxiosResponse, AxiosRequestConfig } from './axios';
+import axios, { AxiosResponse, AxiosRequestConfig } from './axios'; // 自己的库
 
 const baseURL = 'http://localhost:8080';
 // 服务器返回的对象
@@ -7,26 +7,30 @@ interface User {
   name: string;
   password: string;
 }
-let user:User = {
-  name: 'zhangsan',
-  password: '123456'
-}
+
+let user:User = { name: 'zhangsan', password: '123456' }
 
 /*
 * 先看一下怎么使用官方库
+* get
+* post
+* 错误处理（网络错误，超时）
 */
-axios({
-  method: 'post', //'get', // 方法名
-  url: baseURL + '/post', //'/get', // 访问路径
-  // params: user // 查询参数对象，它会转成查询字符串放在？后面
-  headers: {
-    'content-type': 'application/json',
-  }
-}).then((response: AxiosResponse<User>) => {
-  console.log(response)
-  conosle.log(response.data)
-  // return response.data
-})
-.catch((error: any) => {
-  console.log(error)
-})
+// setTimeout(function(){
+  axios({
+    method: 'post', //'get', // 方法名
+    url: baseURL + '/post', //'/get', // 访问路径
+    // params: user // 查询参数对象，它会转成查询字符串放在？后面 （是get请求可以传的参数）
+    headers: {
+      'content-type': 'application/json',
+    }
+  }).then((response: AxiosResponse<User>) => {
+    console.log(response)
+    conosle.log(response.data)
+    // return response.data
+  })
+  .catch((error: any) => {
+    console.log(error)
+  })
+// }, 5000)
+
