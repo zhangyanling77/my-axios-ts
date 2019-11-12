4097a10d521511ee65dccc7dac2a16bf1a3963a9
zhangyanling77 <18783226594@163.com>
2019-11-12 10:35:15 +0800

    post请求中添加header和data的处理

--- src/axios/Axios.tsx
+++ src/axios/Axios.tsx
@@ -10,13 +10,13 @@ export default class Axios {
   // 定义一个派发请求的方法
   dispatchRequest<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return new Promise<AxiosResponse<T>>(function(resolve, reject){
-      let { method, url, params } = config
+      let { method, url, params, headers, data } = config
       let request = new XMLHttpRequest();
       if(params && typeof params == 'object'){
         params = qs.stringify(params)
       }
-      url += ((url.indexOf('?') == -1?'?':'&') + params)
-      request.open(method, url, true)
+      url += ((url!.indexOf('?') == -1?'?':'&') + params)
+      request.open(method!, url!, true)
       request.onreadystatechange = function(){
         if(request.readyState === 4){
           if(request.status >= 200 && request.status < 300){
@@ -35,6 +35,17 @@ export default class Axios {
           }
         }
       }
+      if(headers){
+         for(let key in headers){
+           request.setRequestHeader(key, headers[key])
+         }
+      }
+      let body: string | null = null;
+      if(data){
+         // 转字符串
+        body = JOSN.stringify(data)
+      }
+      request.send()
     })
   }
 }
