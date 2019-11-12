ddbcc0cfb17319c806364c2c5cae71adee4b39c9
zhangyanling77 <18783226594@163.com>
2019-11-12 10:45:41 +0800

    错误处理

--- src/index.tsx
+++ src/index.tsx
@@ -14,16 +14,18 @@ let user:User = { name: 'zhangsan', password: '123456' }
 * 先看一下怎么使用官方库
 * get
 * post
-* 错误处理（网络错误，超时）
+* 错误处理（网络错误，超时错误，状态码错误）
 */
 // setTimeout(function(){
   axios({
     method: 'post', //'get', // 方法名
-    url: baseURL + '/post', //'/get', // 访问路径
+    url: baseURL + '/post_status?code=400', //'/post_timeout?timeout=3000', //'/post', //'/get', // 访问路径
     // params: user // 查询参数对象，它会转成查询字符串放在？后面 （是get请求可以传的参数）
     headers: {
       'content-type': 'application/json',
-    }
+    },
+    timeout: 1000,
+    data: user, // 查询参数，会转成查询字符串放在?的后面 （是post请求需要带的参数）
   }).then((response: AxiosResponse<User>) => {
     console.log(response)
     conosle.log(response.data)
