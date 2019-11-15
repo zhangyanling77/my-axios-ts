9fc753d08817668ede63e193f3c5182314324df1
zhangyanling77 <18783226594@163.com>
2019-11-15 17:42:09 +0800

    feature: 添加取消请求的处理

--- src/axios/Axios.tsx
+++ src/axios/Axios.tsx
@@ -1,11 +1,70 @@
 import { AxiosRequestConfig, AxiosResponse } from './types'
+import AxiosInterceptorManager, { Interceptor } from './AxiosInterceptorManager';
 import qs from 'qs'
 import parseHeader from 'parse-headers'
+let defaults: AxiosRequestConfig = {
+  method: 'get',
+  timeout: 0,
+  headers: {//请求头
+      common: {//针对所有方法的请求生效
+          name: 'requestname',
+          accept: 'application/json'//指定告诉服务器返回JSON格式的数据
+      }
+  },
+  transformRequest: (data: any, headers: any) => {
+      headers['common']['content-type'] = 'application/json';
+      return JSON.stringify(data);
+  },
+  transformResponse: (response: any) => {
+      return response.data;
+  }
+}
+
+let getStyleMethods = ['get', 'head', 'delete', 'options'];// get风格的请求
+getStyleMethods.forEach((method: string) => {
+    defaults.headers![method] = {};
+});
+let postStyleMethods = ['put', 'post', 'patch'];// post风格的请求
+postStyleMethods.forEach((method: string) => {
+    defaults.headers![method] = {
+        'content-type': 'application/json'// 请求体的格式
+    };
+});
+let allMethods = [...getStyleMethods, ...postStyleMethods];
 
 export default class Axios {
+  public defaults: AxiosRequestConfig = defaults;
+  public interceptors = {
+      request: new AxiosInterceptorManager<AxiosRequestConfig>(),
+      response: new AxiosInterceptorManager<AxiosResponse<T>>()
+  }
   // T用来限制响应对象response里data的类型
   request<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.dispatchRequest(config)
+    config.headers = Object.assign(this.defaults.headers, config.headers);
+    if (config.transformRequest && config.data) {
+        config.data = config.transformRequest(config.data, config.headers);
+    }
+    const chain: Array<Interceptor<AxiosRequestConfig> | Interceptor<AxiosResponse<T>>> = [
+        { onFulfilled: this.dispatchRequest }
+    ]
+    //[request1,request]  [request2,request1,request]  [request3,request2,request1,request,]
+    this.interceptors.request.interceptors.forEach((interceptor: Interceptor<AxiosRequestConfig> | null) => {
+        //向数组的左侧添加一个元素
+        interceptor && chain.unshift(interceptor);
+    });
+    //[request3,request2,request1,request,response1,response2,response3]
+    this.interceptors.response.interceptors.forEach((interceptor: Interceptor<AxiosResponse<T>> | null) => {
+        //向数组的左侧添加一个元素
+        //向数组的左侧添加一个元素
+        interceptor && chain.push(interceptor);
+    });
+    let promise: Promise<any> = Promise.resolve(config);
+    //let promise: any = Promise.resolve(config);
+    while (chain.length) {//如果长度大于0
+        const { onFulfilled, onRejected } = chain.shift()!;//unshift向数组的头部增加元素，shift从头部删除元素并且返回这个元素
+        promise = promise.then(onFulfilled, onRejected);
+    }
+    return promise;
   }
   // 定义一个派发请求的方法
   dispatchRequest<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
@@ -39,7 +98,23 @@ export default class Axios {
       }
       if(headers){
          for(let key in headers){
-           request.setRequestHeader(key, headers[key])
+            //common表示所有的请求方法都生效  或者说key是一个方法名
+            /**
+             * {
+             * headers:{
+             *   common:{accept: 'application/json'},
+             *   post:{'content-type':'application/json'}
+             * }
+             * }
+             */
+            if (key === 'common' || allMethods.includes(key)) {
+                if (key === 'common' || key === config.method) {
+                    for (let key2 in headers[key])
+                        request.setRequestHeader(key2, headers[key][key2]);
+                }
+            } else {
+                request.setRequestHeader(key, headers[key]);
+            }
          }
       }
       let body: string | null = null;
@@ -57,6 +132,12 @@ export default class Axios {
         request.ontimeout = function(){
           reject(`Error: timeout of ${timeout}ms exceeded`)
         }
+      }
+      if (config.cancelToken) {
+          config.cancelToken.then((message: string) => {
+              request.abort();
+              reject(message);
+          });
       }
       request.send(body)
     })
