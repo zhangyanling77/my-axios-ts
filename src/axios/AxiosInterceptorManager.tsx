e780692c981c70503f9ea8907639bd4556e8936a
zhangyanling77 <18783226594@163.com>
2019-11-15 17:36:14 +0800

    feature：实现拦截器类

--- src/axios/AxiosInterceptorManager.tsx
+++ src/axios/AxiosInterceptorManager.tsx
@@ -1,10 +1,27 @@
-class AxiosInterceptorManager {
-    // 每当调用use的时候可以向拦截器管理器中添加一个拦截器
-    use() {
+interface OnFulfilled<V> {
+    (value: V): V | PromiseLike<V> | undefined | null
+}//V | PromiseLike<V>) | undefined | null
+interface OnRejected {
+    (error: any): any
+}
+export interface Interceptor<V> {//某一个拦截器
+    onFulfilled?: OnFulfilled<V>;  //成功的回调
+    onRejected?: OnRejected;  //失败的回调
+}
 
+//T 可能AxiosRequestConfig,也可能是AxiosResponse
+export default class InterceptorManager<V> {
+    public interceptors: Array<Interceptor<V> | null> = []
+    use(onFulfilled?: OnFulfilled<V>, onRejected?: OnRejected): number {
+        this.interceptors.push({
+            onFulfilled,
+            onRejected
+        });
+        return this.interceptors.length - 1;
     }
-    // 删除一个拦截器
-    eject() {
-
+    eject(id: number) {
+        if (this.interceptors[id]) {
+            this.interceptors[id] = null;
+        }
     }
 }
